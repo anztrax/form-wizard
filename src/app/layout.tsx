0ead0b33b11c3f780ffb0c0a9edd53034989a5d4
andrew <andrewanantagondo@gmail.com>
2025-11-26 21:46:18 +0700

    feat(layout): implement AppProviders to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AppProviders } from "@/shared/providers/AppProviders";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
@@ -26,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppProviders>
+          {children}
+        </AppProviders>
       </body>
     </html>
   );
