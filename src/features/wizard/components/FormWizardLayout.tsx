60d688ccd59072e39cbb09eaa604f0a342235ede
andrew <andrewanantagondo@gmail.com>
2025-11-28 00:24:42 +0700

    feat(wizard): Revamp UI + styling in FormWizardLayout component

--- src/features/wizard/components/FormWizardLayout.tsx
+++ src/features/wizard/components/FormWizardLayout.tsx
@@ -4,6 +4,10 @@ import { Form } from "@/shared/components/form/Form";
 import { FormProvider } from "react-hook-form";
 import { useFormWizard } from "../hooks/useFormWizard";
 import { Button } from "@/shared/components/button/Button";
+import { Container } from "@/shared/components/container/Container";
+import { Card } from "@/shared/components/card/Card";
+import { ArrowLeft, ArrowRight, SendHorizontal, Trash2 } from "lucide-react";
+import styles from "./FormWizardLayout.module.css";
 
 export function FormWizardLayout() {
   const {
@@ -20,50 +24,60 @@ export function FormWizardLayout() {
   } = useFormWizard();
 
   return (
-    <FormProvider {...formWizard}>
-      <Form
-        layout={'vertical'}
-        gap={'md'}
-        onSubmit={formWizard.handleSubmit(onSubmitForm)}
-      >
-        <h2>{currentStep.label}</h2>
-        {currentStep.render()}
-        <div style={{ marginTop: 16, display: "flex", gap: 8 }}>
-          {formStepIndex > 0 && (
-            <Button type="button" onClick={onPrevButtonClick}>
-              Previous
-            </Button>
-          )}
-
-          {!isLastStepIndex && (
-            <Button type="button" onClick={onNextButtonClick}>
-              Next
-            </Button>
-          )}
-
-          {isLastStepIndex && (
-            <div style={{
-              display: "flex",
-              gap: 8,
-            }}>
-              <Button
-                type="button"
-                variant="danger"
-                onClick={onClearDraftAndReset}
-                disabled={isFormSubmitting}
-              >
-                Clear Draft & Reset
-              </Button>
-              <Button
-                type="submit"
-                disabled={isFormSubmitting}
-              >
-                {isFormSubmitting ? "Submitting..." : "Submit"}
-              </Button>
+    <Container className={styles["form-wizard-layout"]}>
+      <Card className={styles["form-wizard-layout__card"]}>
+        <FormProvider {...formWizard}>
+          <Form
+            layout={'vertical'}
+            gap={'md'}
+            onSubmit={formWizard.handleSubmit(onSubmitForm)}
+            className={styles["form-wizard-layout__form"]}
+          >
+            <div className={styles["form-wizard-layout__content"]}>
+              <h2 className={styles["form-wizard-layout__title"]}>{currentStep.label}</h2>
+              {currentStep.render()}
             </div>
-          )}
-        </div>
-      </Form>
-    </FormProvider>
+            <div className={styles["form-wizard-layout__button-container"]}>
+              {formStepIndex > 0 && (
+                <Button type="button" variant='outline' onClick={onPrevButtonClick}>
+                  <ArrowLeft size={18} />
+                  <span>Prev</span>
+                </Button>
+              )}
+
+              {!isLastStepIndex && (
+                <div className={styles["form-wizard-layout__button-group"]}>
+                  <Button type="button" variant='outline' onClick={onNextButtonClick}>
+                    <span>Next</span>
+                    <ArrowRight size={18} />
+                  </Button>
+                </div>
+              )}
+
+              {isLastStepIndex && (
+                <div className={styles["form-wizard-layout__button-group"]}>
+                  <Button
+                    type="button"
+                    variant="danger"
+                    onClick={onClearDraftAndReset}
+                    disabled={isFormSubmitting}
+                  >
+                    <Trash2 size={18} />
+                    <span>Reset</span>
+                  </Button>
+                  <Button
+                    type="submit"
+                    disabled={isFormSubmitting}
+                  >
+                    <span>{isFormSubmitting ? "Submitting..." : "Submit"}</span>
+                    {!isFormSubmitting && <SendHorizontal size={18} />}
+                  </Button>
+                </div>
+              )}
+            </div>
+          </Form>
+        </FormProvider>
+      </Card>
+    </Container>
   );
 }
