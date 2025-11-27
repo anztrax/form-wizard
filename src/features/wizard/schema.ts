cef92f1fec22f6ae0159a6d5ab92f092ed29c343
andrew <andrewanantagondo@gmail.com>
2025-11-27 21:47:04 +0700

    feat(wizard): update error validation `Step1Schema`, `Step2Schema` in schema.ts file

--- src/features/wizard/schema.ts
+++ src/features/wizard/schema.ts
@@ -2,29 +2,39 @@ import { getValuesFromOptions } from "@/shared/components/select";
 import { z } from "zod";
 import { employmentTypeSelectOptions, roleSelectOptions } from "./resources";
 
-const RoleEnum = z.enum(getValuesFromOptions(roleSelectOptions));
-const EmploymentTypeEnum = z.enum(getValuesFromOptions(employmentTypeSelectOptions));
+const RoleEnum = z.enum(getValuesFromOptions(roleSelectOptions), {
+  error: 'Invalid selection. Please choose valid role',
+});
+const EmploymentTypeEnum = z.enum(getValuesFromOptions(employmentTypeSelectOptions), {
+  error: 'Invalid selection. Please choose valid employment type',
+});
 
 export const Step1Schema = z.object({
   fullName: z.string().min(2, "Full name is required"),
   email: z.email("Invalid email format"),
-  department: z.string().min(1, "Department is required"),
+  department: z.string({
+    error: "Department is required"
+  }).min(1, "Department is required"),
   role: RoleEnum,
   employeeId: z.string().min(1, "Employee ID is missing"),
 });
 
 export const Step2Schema = z.object({
-  photo: z.string().refine((val) => val.startsWith("data:image"), {
+  photo: z.string({
+    message: "Photo is required",
+  }).refine((val) => val.startsWith("data:image"), {
     message: "Photo is required",
   }),
   employmentType: EmploymentTypeEnum,
-  location: z.string().min(1, "Location is required"),
+  location: z.string({
+    error: "Location is required"
+  }).min(1, "Location is required"),
   notes: z.string().optional(),
 });
 
 export const AdminSchema = Step1Schema.extend({
   roleType: z.literal("admin"),
-}).merge(Step2Schema);
+}).extend(Step2Schema.shape);
 
 const OpsSchema = Step2Schema.extend({
   roleType: z.literal("ops"),
